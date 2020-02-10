f5b2af8afd9c1bf9cd8b5df4fd573a670a43945b
kanvhcq <60723603+kanvhcq@users.noreply.github.com>
2020-02-10 15:01:01 +0700

    Update x1.php

--- x1.php
+++ x1.php
@@ -3,10 +3,15 @@ $servername = "localhost";
   $username = "ppdata_kan";
   $password = "Ptc#02290";
   $dbname = "ppdata_kan";
-$mysql = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($mysql, "utf8");
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  mysqli_set_charset($conn, "utf8");
+	mysqli_query($conn, "SET NAMES utf8");
 
-  if ($mysql->connect_error){
-  $errorcode = $mysql->connect_error;
-  print("MySQL(Connection)> ".$errorcode);
-  }
+if (mysqli_connect_error()) 
+{
+	echo "failed to connect to MySQL: " . mysqli_connect_error();
+}
+	
+
+
+  ?>
