f0ca77a74611230c043fc1a9ee7b1e15b1ce047b
kanvhcq <60723603+kanvhcq@users.noreply.github.com>
2020-02-10 13:17:40 +0700

    Update x1.php

--- x1.php
+++ x1.php
@@ -3,7 +3,7 @@ $servername = "localhost";
   $username = "root";
   $password = "Ptc#02290";
   $dbname = "ppdata_kan";
-$mysql = connect($servername, $username, $password, $dbname);
+$mysql = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($mysql, "utf8");
 
   if ($mysql->connect_error){
