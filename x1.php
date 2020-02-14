a673899b3b171a6378fa7e60e2018bb0bedf6a54
kanvhcq <60723603+kanvhcq@users.noreply.github.com>
2020-02-14 13:12:49 +0700

    Update x1.php

--- x1.php
+++ x1.php
@@ -1,17 +1,3 @@
 <?php
-$servername = "http://ppdatacenter.com/phpMyAdmin/index.php";
-  $username = "ppdata_kan";
-  $password = "Ptc#02290";
-  $dbname = "ppdata_kan";
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-  mysqli_set_charset($conn, "utf8");
-	mysqli_query($conn, "SET NAMES utf8");
-
-if (mysqli_connect_error()) 
-{
-	echo "failed to connect to MySQL: " . mysqli_connect_error();
-}
-	
-
-
-  ?>
+ $objConnect = msqli_connect("localhost","ppdata_kan","Ptc#02290") or die(mysql_error());
+  $objDB = mysqli_select_db("ppdata_kan");
