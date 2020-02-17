6600e3788052169ce1fa2f7dc00dfd83b9b315c4
kanvhcq <60723603+kanvhcq@users.noreply.github.com>
2020-02-17 08:29:44 +0700

    Create con.php

---
+++ con.php
@@ -0,0 +1,9 @@
+<?php
+$Setup_Server = ‘http://ppdatacenter.com/phpMyAdmin/’;
+$Setup_User = ‘ppdata_kan’;
+$Setup_Pwd = ‘Ptc#02290’;
+$Setup_Database = ‘ppdata_kan’;
+mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
+mysql_query(“use $Setup_Database”);
+mysql_query(“SET NAMES UTF8”);
+?>
